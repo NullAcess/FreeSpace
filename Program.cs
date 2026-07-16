e1ae4b9801c81a1390702d26c8a53617871a1b9b
NullAcess <153278494+NullAcess@users.noreply.github.com>
2026-07-16 15:43:47 +0300

    Delegate repeat practice

--- Program.cs
+++ Program.cs
@@ -1,22 +1,85 @@
-﻿delegate void GenericDelegate<T>(T value);
+﻿using static Company;
+
+static class Methods
+{
+    public delegate void GenericDelegate<T>(T value);
+
+    public static void OrdinaryMethod(double value)
+    {
+        Console.WriteLine(value);
+    }
+    public static void GenericMethod<T>(T value)
+    {
+        Console.WriteLine(value);
+    }
+}
+
+static class Company
+{
+    public delegate Test Test();
+    public delegate string CompanyDelegate();
+
+
+    private static Test test = TestMethod();
+    public static Test TestMethod()
+    {
+        test = GetOperation();
+        return test;
+    }
+
+    public static Test GetOperation()
+    {
+        return test;
+        //return Google;
+    }
+
+    public static string DoOperation(CompanyDelegate companyDelegate)
+    {
+        return companyDelegate();
+    }
+
+    public static string Microsoft()
+    {
+        string name = "Microsfot";
+        return name;
+    }
+
+    public static string Google()
+    {
+        string name = "Google";
+        return name;
+    }
+
+    public static string Amazon()
+    {
+        string name = "Amazon";
+        return name;
+    }
+}
 
 class Program
 {
-    static void OrdinaryMethod(double value)
-    {
-        Console.WriteLine(value);
-    }
-    static void GenericMethod<T>(T value)
-    {
-        Console.WriteLine(value);
-    }
+
     static void Main()
     {
-        GenericDelegate<double> genericDelegate = OrdinaryMethod;
-        genericDelegate += GenericMethod;
+        Methods.GenericDelegate<double> genericDelegate = Methods.OrdinaryMethod;
+        genericDelegate += Methods.GenericMethod;
 
-        genericDelegate(5.6136);
+        genericDelegate(5.4321);
         // Console.WriteLine(genericDelegate(5.6136)); to works it you should set return type in your delegate ( Bad: delegate void GenericDelegate<T>(T value); )
         //                                                                                                     ( Good delegate T GenericDelegate<T>(T value);    )
+
+        string company = String.Empty;
+
+        company = Company.DoOperation(Company.Amazon);
+        company = Company.DoOperation(Company.Google);
+        company = Company.DoOperation(Company.Microsoft);
+
+        Console.WriteLine(company);
+        Console.WriteLine();
+        //CompanyDelegate companyDelegate = Company.GetOperation();
+
+        Console.WriteLine();
+        Company.TestMethod();
     }
 }
