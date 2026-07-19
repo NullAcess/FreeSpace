0b81df42d5dae502221eea593280b10b9d92df31
NullAcess <153278494+NullAcess@users.noreply.github.com>
2026-07-19 21:34:23 +0300

    Delegate Covariance and Contravariance

--- Program.cs
+++ Program.cs
@@ -1,44 +1,55 @@
-﻿/*
-👤 Создать класс Character.
-
-🩸 Добавить в него приватное поле int _health = 100.
-
-📜 Объявить кастомный делегат HealthChangedHandler, который возвращает void и принимает один параметр int newHealth.
-
-🔔 Внутри класса Character объявить событие OnHealthChanged на основе созданного делегата.
-*/
-
-delegate void HealthChangedHandler(int health);
-
-class Character
-{
-    private int _health;
-    public event HealthChangedHandler? HealthChanged;
-
-    public Character(int health)
-    {
-        _health = health;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        _health -= damage;
-        HealthChanged?.Invoke(_health);
-    }
-}
+﻿delegate void MakeTransport(Car car);
+delegate Transport Order();
 
 class Program
 {
-    static void HealthDisplay(int health)
-    {
-        Console.WriteLine($"Character new health: {health}");
-    }
-
     static void Main()
     {
-        var hero = new Character(100);
-        hero.HealthChanged += HealthDisplay;
+        Factory factory = new Factory();
+        factory.makeTransport += factory.CarStage;
 
-        hero.TakeDamage(20);
+        Car car = factory.CarFactory("bmw", 400);
+        Console.WriteLine($"car test: | name: {car.Name}, car power: {car.Power}");
+    }
+}
+
+class Factory
+{
+    public event MakeTransport? makeTransport;
+    public event Order? order;
+
+    public Car CarFactory(string name, int power)
+    {
+        Console.WriteLine($"Standart version power: {power}");
+        var fabricCar = new Car(name, power);
+        makeTransport?.Invoke(fabricCar);
+        return fabricCar;
+    }
+
+    public void CarStage(Transport transport)
+    {
+        transport.Power += 50;
+        Console.WriteLine($"Ultra version activated");
+    }
+}
+
+class Transport
+{
+    public string Name { get; }
+    public int Power { get; set;  }
+
+    public Transport(string name, int power)
+    {
+        Name = name;
+        Power = power;
+    }
+}
+
+class Car : Transport
+{
+    public Guid CID { get; }
+    public Car(string name, int power) : base(name, power)
+    {
+        CID = Guid.NewGuid();
     }
 }
