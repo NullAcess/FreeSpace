795dea377bfbc171c635eeb77425d3a7831c683e
NullAcess <153278494+NullAcess@users.noreply.github.com>
2026-07-20 01:11:18 +0300

    Closure practice

--- Program.cs
+++ Program.cs
@@ -1,85 +1,27 @@
-﻿class Factory
-{
-    public event Action<Car>? makeTransport;
-    public event Func<string, int, Transport>? order;
-    public event Predicate<Transport>? isUltraVersion;
-
-    public bool VersionCheck(Transport transport)
-    {
-        if (transport.Power < 400)
-        {
-            Console.WriteLine("Is your car ultra: false"); 
-            return false;
-        }
-
-        Console.WriteLine("Is your car ultra: true");
-        return true;
-    }
-
-    public Transport? ExecuteOrder(string name, int power)
-    {
-        return order?.Invoke(name, power);
-    }
-
-    public Car FactoryConsume(string name, int power)
-    {
-        // выбор пользователя тут допустим switch и выбрал сделать машину.
-        var car = new Car(name, power);
-        Console.WriteLine($"Version power: {power}");
-
-        makeTransport?.Invoke(car);
-        return car;
-    }
-
-    public void PrintStatus(Transport vehicle)
-    {
-        Console.WriteLine($"Making and stage your car");
-    }
-
-    public void PrintStageImprove(Transport vehicle)
-    {
-        vehicle.Power += 50;
-        Console.WriteLine($"Ultra version activated");
-        isUltraVersion?.Invoke(vehicle);
-    }
-}
-
-class Transport
-{
-    public string Name { get; }
-    public int Power { get; set;  }
-
-    public Transport(string name, int power)
-    {
-        Name = name;
-        Power = power;
-    }
-}
-
-class Car : Transport
-{
-    public Guid CID { get; }
-    public Car(string name, int power) : base(name, power)
-    {
-        CID = Guid.NewGuid();
-    }
-}
+﻿delegate Closure Closure();
 
 class Program
 {
+    static Closure OutFunc()
+    {
+        int x = 5;
+
+        Closure Inner()
+        {
+            x++;
+            Console.WriteLine(x);
+            return Inner;
+        }
+
+        return Inner;
+    }
+
     static void Main()
     {
-        Factory factory = new Factory();
-        factory.order += factory.FactoryConsume;
-        factory.makeTransport += factory.PrintStatus;
-        factory.makeTransport += factory.PrintStageImprove;
-        factory.isUltraVersion += factory.VersionCheck;
+        Closure func = OutFunc();
 
-        Transport? car = factory.ExecuteOrder("bmw", 400);
-
-        if(car is Car displayCar)
-        {
-            Console.WriteLine($"car test: | name: {displayCar.Name}, car power: {displayCar.Power}, car CID: {displayCar.CID}");
-        }
+        func(); 
+        func(); 
+        func(); 
     }
 }
