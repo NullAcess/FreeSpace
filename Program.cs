408f6d0e6e629ba5513ba1193bb3f1c37428c1d5
NullAcess <153278494+NullAcess@users.noreply.github.com>
2026-07-22 22:07:07 +0300

    Compare practice

--- Program.cs
+++ Program.cs
@@ -1,32 +1,27 @@
-﻿interface IMyCloneable<T>
+﻿class Player : IComparable<Player>
 {
-    T Clone();
-}
+    public string Nickname { get; private set; }
+    public int Score { get; private set; }
+    public int Level { get; private set; }
 
-class OrderItem
-{
-    public string Title { get; private set; }
-    public decimal Price { get; private set; }
-
-    public OrderItem(string title, decimal price)
+    public Player(string nickname, int score, int level)
     {
-        Title = title;
-        Price = price;
+        Nickname = nickname;
+        Score = score;
+        Level = level;
     }
-}
 
-class Order : IMyCloneable<Order>
-{
-    public int Id { get; set; }
-    public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
-
-    public Order Clone()
+    public int CompareTo(Player other)
     {
+        if (other == null) return 1;
         
-        Order order = (Order)MemberwiseClone();
-        order.Items = Items.Select(item => new OrderItem(item.Title, item.Price)).ToList();
+        int scoreResult = other.Score.CompareTo(Score);
+        if(scoreResult != 0)
+        {
+            return scoreResult;
+        }
 
-        return order;
+        return Nickname.CompareTo(other.Nickname);
     }
 }
 
@@ -34,30 +29,20 @@ class Program
 {
     static void Main()
     {
-        var order1 = new Order();
-        order1.Id = 5;
-        Console.WriteLine(order1.Id);
-        var mouse = new OrderItem("mouse", 20);
-        var laptop = new OrderItem("laptop", 500);
+        var leaderboard = new List<Player>
+            {
+                new Player("Shadow", 2500, 50),
+                new Player("Alex", 3100, 70),
+                new Player("CyberNinja", 2500, 45),
+                new Player("Alpha", 3100, 80),
+                new Player("Beginner", 1000, 10)
+            };
 
-        order1.Items.Add(mouse);
-        order1.Items.Add(laptop);
+        leaderboard.Sort();
 
-        var order2 = new Order();
-        order2 = order1?.Clone();
-        order2?.Items.Add(laptop);
-        order2?.Id = 6;
-        Console.WriteLine(order2?.Id);
-        Console.WriteLine(order1?.Id);
-
-        for (int i = 0; i < order2?.Items?.Count; i++)
+        foreach(Player user in leaderboard)
         {
-            Console.Write($"{order2.Items[i].Title} ");
-        }
-        Console.WriteLine();
-        for (int i = 0; i < order1?.Items?.Count; i++)
-        {
-            Console.Write($"{order1.Items[i].Title} ");
+            Console.WriteLine($"{user.Nickname} | {user.Score} | {user.Level}");
         }
     }
 }
