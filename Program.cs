a01e941c5e9520e9a7b15dcacc42bf0a0087e6fc
NullAcess <153278494+NullAcess@users.noreply.github.com>
2026-08-31 11:53:53 +0300

    Write / save json file ( with commas and blocks )

--- Program.cs
+++ Program.cs
@@ -1,29 +1,151 @@
-﻿class Program
-{
-    static void Main()
-    {
-        var obj = new object();
-        int x = 0;
+﻿using System.Text.Json;
 
-        for (int i = 0; i < 5; i++)
+class Player
+{
+    public string UserName { get; } = "Unknown";
+    public int Id { get; }
+
+    public Player(string userName, int id)
+    {
+        UserName = userName;
+        Id = id;
+    }
+}
+
+class Program
+{
+    private const string FilePath = @"C:\Users\ll1on\Desktop";
+    private static readonly string CombinePath = Path.Combine(FilePath, "user.json");
+    private static readonly JsonSerializerOptions jsonSerializerOptions = new() { WriteIndented = true };
+    private static List<Player> _players = new();
+
+    static bool CompareNodes(Player player, Player nodePlayer)
+    {
+        if (player.UserName == nodePlayer.UserName)
         {
-            Thread myThread = new(Print);
-            myThread.Name = $"Thread {i}";
-            myThread.Start();
+            UserNotification("Name is already exist");
+            return false;
         }
 
-        void Print()
+        if (player.Id == nodePlayer.Id)
         {
-            lock (obj)
+            UserNotification("Name is already exist");
+            return false;
+        }
+
+        return true;
+    }
+
+    static async Task<bool> CheckSave(Player player)
+    {
+        {
+            using (FileStream fs = new FileStream(CombinePath, FileMode.Open))
             {
-                x = 1;
-                for (int i = 0; i < 5; i++)
+                List<Player> nodePlayer = await JsonSerializer.DeserializeAsync<List<Player>>(fs, jsonSerializerOptions);
+
+                for (int i = 0; i < _players.Count; i++)
                 {
-                    Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
-                    x++;
-                    Thread.Sleep(100);
+                    if (!CompareNodes(player, nodePlayer[i]))
+                    {
+                        UserNotification("Check save: FALSE");
+                        return false;
+                    }
+                }
+
+                UserNotification("Check save: true");
+                return true;
+            }
+        }
+    }
+
+    static async Task<bool> SaveToJson(Player player)
+    {
+        {
+            if (!await CheckSave(player))
+            {
+                return false;
+            }
+            else
+            {
+                _players.Add(player);
+                using (FileStream fs = new FileStream(CombinePath, FileMode.Create, FileAccess.Write))
+                {
+                    await JsonSerializer.SerializeAsync(fs, _players, jsonSerializerOptions);
+                    return true;
                 }
             }
         }
+    }
+
+    static async Task InitializeDefaultFile()
+    {
+        while (true)
+        {
+            var defaultPlayer = new Player("Unknown", -1);
+
+            _players.Add(defaultPlayer);
+            using (FileStream fs = new FileStream(CombinePath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                await JsonSerializer.SerializeAsync(fs, _players, jsonSerializerOptions);
+                return;
+            }
+        }
+    }
+
+    static async Task Main()
+    {
+        await InitializeDefaultFile();
+
+        string userName = "Unknown";
+        int id = -1;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.Write("Write username: ");
+            userName = Console.ReadLine() ?? String.Empty;
+            Console.Write("Write id: ");
+            int.TryParse(Console.ReadLine() ?? String.Empty, out id);
+
+            var player = new Player(userName, id);
+
+            Console.WriteLine("1. Save character");
+            Console.WriteLine("2. Save check");
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.D1: await SaveToJson(player); continue;
+                case ConsoleKey.D2: await CheckSave(player); continue;
+                case ConsoleKey.D3: DisplayListOfPlayers(); continue;
+                case ConsoleKey.Backspace:; return;
+            }
+        }
+    }
+
+    static void DisplayListOfPlayers()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("===============================================================");
+
+        for(int i =0; i < _players.Count; i++)
+        {
+            Console.WriteLine($"Name: {_players[i].UserName} | Id: {_players[i].Id}");
+        }
+
+        Console.WriteLine("===============================================================");
+        Console.ResetColor();
+        Console.ReadKey(true);
+        return;
+    }
+
+    static void UserNotification(string message)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(message);
+        Thread.Sleep(1000);
+        Console.ResetColor();
+        Console.Clear();
     }
 }
